926a0260aefee4881d7c4a8a5dbda6625489dd94
Adriano Alecrim <adrianoenrico1@gmail.com>
2021-04-02 21:21:10 +0100

    created backdrop comp

---
+++ frontend/src/components/backdrop.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: '#fff',
+    },
+}));
+
+//This component is a simple backdrop that can be thrown in logic for loading animations
+export default function SimpleBackdrop() {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(true);
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Backdrop className={classes.backdrop} open={open} onClick={handleClose}>
+                <CircularProgress color="inherit" />
+            </Backdrop>
+        </div>
+    );
+}

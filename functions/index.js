583b52b0b05a7102a850a99464895c4e940a34be
Adriano Alecrim <adrianoenrico1@gmail.com>
2021-04-02 22:00:45 +0100

    created express apollo server for basic query

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,41 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const {ApolloServer, gql} = require("apollo-server-express");
+
+
+const app = express();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Bellow is my schema for my birthday boi, their queries and mutations.
+const typeDefs = gql`
+  type BirthdayBoi {
+    birthday: String!
+    name: String!
+    country: String!
+  }
+
+  type Query {
+    birthdayBois: [BirthdayBoi]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    birthdayBois: ()=> initialBois,
+  },
+};
+
+const initialBois = [
+  {
+    name: "Adriano Alecrim",
+    country: "Portugal",
+    birthday: "08/11/1998",
+    id: 1,
+  },
+];
+
+const server = new ApolloServer({typeDefs, resolvers, playground: true});
+server.applyMiddleware({app, path: "/", cors: true});
+
+exports.graphql = functions.https.onRequest(app);

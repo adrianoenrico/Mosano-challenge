ddf3d26e7c62607ace08a28c42a58b59a1dfd936
Adriano Alecrim <adrianoenrico1@gmail.com>
2021-04-03 02:19:47 +0100

    added mongoDB datasource

--- functions/index.js
+++ functions/index.js
@@ -1,54 +1,121 @@
 const functions = require("firebase-functions");
 const express = require("express");
-const {ApolloServer, gql} = require("apollo-server-express");
+const mongoose = require("mongoose");
+const {
+  ApolloServer,
+  gql,
+  AuthenticationError,
+} = require("apollo-server-express");
+mongoose.connect(
+    "mongodb+srv://adriano:7kqA6_Hh9nhSP3p@cluster0.t0ahj.mongodb.net/mosano?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+);
 
 
-const app = express();
+// Mongoose Schemas
+const countrySchema = new mongoose.Schema({
+  name: String,
+  id: mongoose.ObjectId,
+});
+const Country = mongoose.model("Country", countrySchema);
+const birthdayBoiSchema = new mongoose.Schema({
+  name: String,
+  birthday: String,
+  country: String,
+  id: mongoose.ObjectId,
+});
+const BirthdayBoi = mongoose.model("BirthdayBoi", birthdayBoiSchema);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-// Bellow is my schema for my birthday boi, their queries and mutations.
+// GraphQL Defs
 const typeDefs = gql`
   type BirthdayBoi {
     birthday: String!
     name: String!
-    country: String!
-    id: ID!
+    country: Country!
+    _id: ID!
   }
   
   type Country {
-      name: String!
+      name: String!,
+      _id: ID!
   }
 
   type Query {
     birthdayBois: [BirthdayBoi]
     countries : [Country]
   }
+  type Mutation {
+    addBirthdayBoi(
+        name: String!
+        birthday: String!
+        country: String!
+    ): BirthdayBoi,
+    addCountry(name: String!): Country,
+    deleteCountry(_id: ID!): Country,
+    updateCountry(_id: ID!, name: String!): Country,
+  }
 `;
-
 const resolvers = {
   Query: {
-    birthdayBois: ()=> initialBois,
-    countries: ()=> countries,
+    birthdayBois: async () => await BirthdayBoi.find(),
+    countries: async () => await Country.find(),
+  },
+  Mutation: {
+    addBirthdayBoi: async (parent, args) => {
+      // This mut isnt protected bc we want a public app
+      const boi = new BirthdayBoi({
+        name: args.name,
+        country: args.country,
+        birthday: args.birthday,
+      });
+      return await boi.save();
+    },
+    addCountry: async (parent, args, context) => {
+      // This is protected by context verification.
+      if (!context.loggedIn) {
+        throw new AuthenticationError("AUTH TOKEN NOT FOUND OR NOT VALID");
+      }
+      const country = new Country({
+        name: args.name,
+      });
+      return await country.save();
+    },
+    deleteCountry: async (parent, args, context) => {
+    // This is protected by context verification.
+      if (!context.loggedIn) {
+        throw new AuthenticationError("AUTH TOKEN NOT FOUND OR NOT VALID");
+      }
+      const country = await Country.findOneAndDelete({_id: args._id});
+      return country;
+    },
+    updateCountry: async (parent, args, context) => {
+    // This is protected by context verification.
+      if (!context.loggedIn) {
+        throw new AuthenticationError("AUTH TOKEN NOT FOUND OR NOT VALID");
+      }
+      const country = await Country.findOneAndUpdate(
+          {_id: args._id},
+          {name: args.name}
+      );
+      return country;
+    },
   },
 };
 
-const initialBois = [
-  {
-    name: "Adriano Alecrim",
-    country: "Portugal",
-    birthday: "08/11/1998",
-    id: 1,
+// Express/Apollo Startup and connection
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({req}) => {
+    const loggedIn = req.headers.authorization?true:false;
+    return {loggedIn};
   },
-];
+});
+server.applyMiddleware({app, path: "/"});
 
-const countries = [
-  {
-    name: "Portugal",
-  },
-];
-
-const server = new ApolloServer({typeDefs, resolvers, playground: true});
-server.applyMiddleware({app, path: "/", cors: true});
 
 exports.graphql = functions.https.onRequest(app);

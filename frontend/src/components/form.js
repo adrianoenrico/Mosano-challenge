e6f88c2728251b8c8069bc16c732cdda62e26100
Adriano Alecrim <adrianoenrico1@gmail.com>
2021-04-04 03:14:31 +0100

    formatt + error handling in mat-ui

--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -9,6 +9,7 @@ import InputLabel from '@material-ui/core/InputLabel'
 import FormControl from '@material-ui/core/FormControl'
 import Typography from '@material-ui/core/Typography'
 import LinearProgress from '@material-ui/core/LinearProgress';
+import FormHelperText from '@material-ui/core/FormHelperText'
 //Data
 import { useQuery, gql, useMutation } from '@apollo/client';
 // Other
@@ -18,23 +19,20 @@ import { getAge } from '../utils'
 
 const useStyles = makeStyles((theme) => ({
     container: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      padding: theme.spacing(2),
+        display: 'flex',
+        flexWrap: 'wrap',
+        padding: theme.spacing(2),
     },
     textField: {
-     margin: theme.spacing(1)
+        margin: theme.spacing(1)
     },
-    btnContainer:{
+    btnContainer: {
         justifyContent: 'flex-end',
         display: 'flex',
         width: '100%',
-        padding: theme.spacing(3,1,0,0)
+        padding: theme.spacing(3, 1, 0, 0)
     },
-    errorMsg:{
-        marginLeft: theme.spacing(1)
-    }
-  }));
+}));
 
 const GET_COUNTRIES = gql`
   query Countries {
@@ -67,11 +65,11 @@ export default function Form() {
             pollInterval: 30000,
         }
     );
-    const [addBirthdayBoi] = useMutation(ADD_BIRTHDAY, { 
-        update(cache, { data: { addBirthdayBoi }}) {
+    const [addBirthdayBoi] = useMutation(ADD_BIRTHDAY, {
+        update(cache, { data: { addBirthdayBoi } }) {
             cache.modify({
                 fields: {
-                    birthdayBois(existingBois = []){
+                    birthdayBois(existingBois = []) {
                         const newBoiRef = cache.writeFragment({
                             data: addBirthdayBoi,
                             fragment: gql`
@@ -95,20 +93,20 @@ export default function Form() {
     const onSubmit = data => {
         let birthday = new Date(data.birthday).toLocaleDateString('en-US')
         addBirthdayBoi({
-            variables:{
-                name: `${data.name}${data.surname?' ' + data.surname:''}`,
+            variables: {
+                name: `${data.name}${data.surname ? ' ' + data.surname : ''}`,
                 birthday: birthday,
                 country: data.country
             }
         })
         let age = getAge(birthday);
         let bday = new Date(data.birthday);
-        setSnack({ 
-          open: true,
-          message: `
+        setSnack({
+            open: true,
+            message: `
             Hello ${data.name} from ${data.country} on 
             ${bday.getDate()} of month
-            ${bday.toLocaleDateString('en-US', {  month: 'long'})} you will
+            ${bday.toLocaleDateString('en-US', { month: 'long' })} you will
             have ${age + 1}
           `
         });
@@ -117,36 +115,36 @@ export default function Form() {
     return (
         <>
             <form className={classes.container} onSubmit={handleSubmit(onSubmit)}>
-                <TextField className={classes.textField} label='Name' name='name' placeholder='first name goes here' inputRef={register({required: true})} fullWidth />
-                {errors.name && <Typography className={classes.errorMsg} variant='subtitle2' color='error'>A first name is required</Typography>}
+                <TextField error={errors.name} helperText={errors.name ? 'A first name is required' : false} className={classes.textField} label='Name' name='name' placeholder='first name goes here' inputRef={register({ required: true })} fullWidth />
                 <TextField className={classes.textField} label='Surname' name='surname' placeholder='surname goes here' inputRef={register} fullWidth />
-                <FormControl className={classes.textField} fullWidth>
+                <FormControl className={classes.textField} fullWidth error={errors.country}>
                     <InputLabel htmlFor="country-select">
                         Country
                     </InputLabel>
                     <Controller
                         name='country'
                         control={control}
-                        rules={{required: true}}
+                        rules={{ required: true }}
                         defaultValue={''}
                         as={
                             <Select id='country-select' >
-                                {loading && 
+                                {loading &&
                                     <div className={classes.loadingItems}>
-                                        <LinearProgress  />
+                                        <LinearProgress />
                                         <Typography align='center' component='div'>Loading data...</Typography>
                                     </div>
                                 }
-                                {data && data.countries.map((option,i)=>(
+                                {data && data.countries.map((option, i) => (
                                     <MenuItem key={`${option.name}-${i}`} value={option.name}>{option.name}</MenuItem>
                                 ))}
                             </Select>
                         }
                     />
+                    {errors.country &&
+                        <FormHelperText>Please select a country from the dropdown list</FormHelperText>
+                    }
                 </FormControl>
-                {errors.country && <Typography className={classes.errorMsg} variant='subtitle2' color='error'>Please select a country from the dropdown list</Typography>}
-                <TextField className={classes.textField} label='Birthday' name='birthday' type='date' inputRef={register({required: true})} fullWidth InputLabelProps={{shrink: true}}/>
-                {errors.birthday && <Typography className={classes.errorMsg} variant='subtitle2' color='error'>Please enter your birthday</Typography>}
+                <TextField className={classes.textField} label='Birthday' name='birthday' type='date' inputRef={register({ required: true })} fullWidth InputLabelProps={{ shrink: true }} error={errors.birthday} helperText={errors.country ? 'Please enter a valid date' : false} />
                 <div className={classes.btnContainer}>
                     <Button className={classes.submitBtn} variant='contained' color='primary' type='submit'>Save</Button>
                 </div>

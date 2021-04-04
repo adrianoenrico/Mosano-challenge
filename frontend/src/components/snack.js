17929a71f5d659fd88dc20fda24ba5c0a7e3ec5a
Adriano Alecrim <adrianoenrico1@gmail.com>
2021-04-04 02:07:38 +0100

    added snack component

---
+++ frontend/src/components/snack.js
@@ -0,0 +1,29 @@
+import React,  { useState } from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
+
+export default function Snack({ open, message, handleClose }) {
+  return (
+    <div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message={message}
+        action={
+          <>
+            <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </>
+        }
+      />
+    </div>
+  );
+}
